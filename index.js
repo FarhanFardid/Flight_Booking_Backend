1a34d702a8328f68671720c93cbab2c3dc899413
FarhanFardid <farhanfardid009@gmail.com>
2024-11-28 11:29:18 +0600

    Initial backend setup with express and intalled packages of cors, mongodb, dotenv

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+//----------- Middlewares -------------
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send("Server is Running");
+})
