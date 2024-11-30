9b42c54a2ea495919e0a492da217efe4c3cc9d8a
FarhanFardid <farhanfardid009@gmail.com>
2024-11-30 12:19:53 +0600

    authMiddleware added and jwt secret key added to .env file

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+// Middleware to verify JWT
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  try {
+    // Verifying token using the secret key
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token." });
+  }
+};
+
+module.exports = authMiddleware;
