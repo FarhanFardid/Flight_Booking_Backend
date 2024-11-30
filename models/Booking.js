c8ff56ac819bc5e7e50bfd4a5089343446e313dd
FarhanFardid <farhanfardid009@gmail.com>
2024-11-30 11:45:05 +0600

    Mongoose flight and booking schema implemented

---
+++ models/Booking.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    flightId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Flight",
+      required: true,
+    },
+    numberOfSeats: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    bookingStatus: {
+      type: String,
+      enum: ["Confirmed", "Pending", "Cancelled"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
