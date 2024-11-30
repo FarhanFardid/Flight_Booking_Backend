f6d12e0ef060888f7a883fd57aff77d321c46883
FarhanFardid <farhanfardid009@gmail.com>
2024-11-30 11:15:23 +0600

    MongoDB conncetion successfull using mongoose

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 require("dotenv").config();
+
+const app = express();
 
 //----------- Middlewares -------------
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+// MongoDb connection string from .env
+const mongoURI = process.env.MONGO_URI;
+
+// Connection to MongoDB using Mongoose
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Flight Booking Server is Running");
