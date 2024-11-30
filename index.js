4e9586bb82a867cb31e827f8192c95017e6da49e
FarhanFardid <farhanfardid009@gmail.com>
2024-11-30 13:21:15 +0600

    reg and login routes implemented with authcCOntroller

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
+const router = express.Router();
 require("dotenv").config();
 
 const app = express();
@@ -14,9 +15,12 @@ app.use(express.json());
 const mongoURI = process.env.MONGO_URI;
 
 // Schema Models
-const User = require('./models/User');
-const Flight = require('./models/Flight');
-const Booking = require('./models/Booking');
+const User = require("./models/User");
+const Flight = require("./models/Flight");
+const Booking = require("./models/Booking");
+const { registerUser, loginUser } = require("./controllers/authController");
+
+
 
 // Connection to MongoDB using Mongoose
 mongoose
@@ -27,6 +31,11 @@ mongoose
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
+
+  // Routes
+// User reg and Login Auth routes
+router.post("/api/register", registerUser);
+router.post("/api/login", loginUser);
 
 app.get("/", (req, res) => {
   res.send("Flight Booking Server is Running");
